f5992880096a57ef3b03b39f2e1ebb744dc390c0
wangjunchao <378350151@qq.com>
2018-12-25 15:13:30 +0800

    wjc

--- application/controllers/serve.php
+++ application/controllers/serve.php
@@ -7,7 +7,35 @@ class Serve extends CI_Controller {
 
 //    服务分类列表
     public function index(){
-        $this->load->view('/settings/serve');
+        $data = [];
+        $t = 0;
+        $user = $this->session->userdata('jxcsys');
+
+        if($user['orgLevel'] == 3){
+            $array = array('level' => 1, 'lowId' =>$user['orgId']);
+        }elseif ($user['orgLevel'] == 2){
+            $array = array('level' => 1, 'midId' =>$user['orgId']);
+        }elseif ($user['orgLevel'] == 1){
+            $array = array('level' => 1, 'topId' =>$user['orgId']);
+        }
+        $one = $this->db->where($array)->get('ci_serve')->result();
+
+        foreach ($one as $k=>$v){
+            $data[$t]['id']= $v->id;
+            $data[$t]['name']= $v->name;
+            $two = $this->db->where(['parentId'=>$v->id])->get('ci_serve')->result();
+            foreach ($two as $key => $value){
+                $data[$t]['child']['id']= $value->id;
+                $data[$t]['child']['name']= $value->name;
+                $three = $this->db->where(['parentId'=>$value->id])->get('ci_serve')->result();
+                foreach ($three as $a=>$b){
+                    $data[$t]['child']['child']['id']= $b->id;
+                    $data[$t]['child']['child']['name']= $b->name;
+                }
+            }
+        }
+
+        $this->load->view('/settings/serve',['data'=>$data]);
     }
 
     //    VIP卡添加
