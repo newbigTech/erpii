f686259eca66190a717918eae3562220638c0834
xzc <1805793040@qq.com>
2018-12-25 15:14:00 +0800

    u的帕特

--- application/views/settings/service.php
+++ application/views/settings/service.php
@@ -64,9 +64,12 @@ $(document).keydown(function(event) {
         border-bottom: 1px solid #e2e2e2;
         border-top: 1px solid #fff;
         border-left: 1px solid #fff;
-        width: 100px;
+        /*width: 100px;*/
         height: 33px;
         text-align: center;
+    }
+    .table td:last-child{
+        text-align: left;
     }
     .table tr:hover{
         background-color: #f8ff94;
@@ -79,6 +82,19 @@ $(document).keydown(function(event) {
         margin-bottom: -6px;
         overflow: hidden;
         text-overflow:ellipsis;
+    }
+    .table .write,
+    .table .delete{
+        display: inline-block;
+        width: 16px;
+        height: 16px;
+        background-image: url(<?php echo base_url()?>statics/css/img/ui-icons_20150410.png);
+    }
+    .table .write{
+        background-position: -112px -16px;
+    }
+    .table .delete{
+        background-position: -64px -16px;
     }
     #page{
         position: absolute;
@@ -139,14 +155,86 @@ $(document).keydown(function(event) {
         height: 16px;
         margin-top: 8px;
     }
+    #add{
+        position: fixed;
+        width: 402px;
+        height: 218px;
+        background-color: #fff;
+        top: 40%;
+        left: 50%;
+        transform: translate(-50%,-50%);
+        box-shadow: 1px 1px 10px 10px #a9a9a9;
+        border-radius: 3px;
+        z-index: 1998;
+    }
+    #add>#add_header{
+        background-color: #f5f5f5;
+        height: 32px;
+        width: 100%;
+        border-radius: 3px;
+    }
+    #add>#add_header>#add_title{
+        float: left;
+        height: 32px;
+        line-height: 32px;
+        font-size: 14px;
+        font-weight: 700;
+        margin-left: 10px;
+    }
+    #add>#add_header>#add_close{
+        float: right;
+        height: 32px;
+        line-height: 32px;
+        color: #aaa;
+        font-size: 18px;
+        width: 20px;
+        cursor: pointer;
+    }
+    #add>#add_content{
+        width: 100%;
+        height: 100%;
+        box-sizing: border-box;
+    }
+    #add>#add_content>.content_main{
+        width: 320px;
+        height: 112px;
+        position: relative;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%,-70%);
+        box-sizing: border-box;
+    }
+    #add>#add_content>.content_main>li{
+        width: 100%;
+        margin-bottom: 10px;
+    }
+    #add>#add_content>.content_main>li>span{
+        display: inline-block;
+        width: 70px;
+        height: 28px;
+        font-size: 14px;
+    }
+    #add>#add_content>.content_main>li>input{
+        width: 212px;
+        height: 28px;
+        display: inline-block;
+        border: 1px solid #ddd;
+    }
+    #add_footer{
+        position: absolute;
+        width: 402px;
+        height: 33px;
+        bottom: 0;
+        right: 0;
+    }
 </style>
 </head>
 <body>
 <div class="wrapper">
 	<div class="mod-search cf">
 	    <div class="fr">
-            <a href="javascript:void(0);" class="ui-btn ui-btn-sp mrb">新增</a>
-            <a href="javascript:void(0);" class="ui-btn" id="btn-batchDel">刷新</a>
+            <a href="javascript:void(0);" id="new" class="ui-btn ui-btn-sp mrb">新增</a>
+            <a href="javascript:void(0);" class="ui-btn" id="refresh">刷新</a>
         </div>
 	  </div>
     <div class="grid-wrap">
@@ -160,7 +248,19 @@ $(document).keydown(function(event) {
                 </thead>
                 <tbody>
                     <tr>
+                        <td>
+                            <span class="write"></span>
+                            <span class="delete"></span>
+                            <input type="hidden" value="1">
+                        </td>
                         <td><span>2hgtr4weh4efe3gerrgrwhtbvrgweh56t56t4</span></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <span class="write"></span>
+                            <span class="delete"></span>
+                            <input type="hidden" value="2">
+                        </td>
                         <td><span>2hgtr4weh4efe3gerrgrwhtbvrgweh56t56t4</span></td>
                     </tr>
                 </tbody>
@@ -190,9 +290,52 @@ $(document).keydown(function(event) {
     </div>
 </div>
 
+
+<div id="ldg_lockmask" style="position: fixed; left: 0px; top: 0px; width: 100%; height: 100%; overflow: hidden; z-index: 1977;display: none;"></div>
+<div id="add" style="display: none;">
+    <div id="add_header" class="clearfix">
+        <div id="add_title">新增服务类别</div>
+        <div id="add_close" class="close_add">&times;</div>
+    </div>
+    <div id="add_content">
+        <ul class="content_main clearfix">
+            <li><span>上级分类:</span><input type="text" id="car_name"></li>
+            <li><span>分类:</span><input type="text" id="car_num"></li>
+        </ul>
+    </div>
+    <div id="add_footer">
+        <td colspan="2">
+            <div class="ui_buttons">
+                <input type="hidden" value="" id="id">
+                <input type="button" id="save" value="保存" class="ui_state_highlight" />
+                <input type="button" class="close_add" value="关闭" />
+            </div>
+        </td>
+    </div>
+</div>
+
 <script>
     $(function () {
-        
+        $('#new').on('click',function () {
+            $('#ldg_lockmask').css('display','');
+            $('#add').css('display','');
+            $('#type').val('add');
+        });
+        $('.close_add').on('click',function () {
+            $('#ldg_lockmask').css('display','none');
+            $('#add').css('display','none');
+        });
+        $('.delete').on('click',function () {
+            var id = $(this).parent().find('input').val();
+
+        });
+        $('.write').on('click',function () {
+            var id = $(this).parent().find('input').val();
+
+        });
+        $('#refresh').on('click',function () {
+
+        })
     });
 </script>
 <script>
