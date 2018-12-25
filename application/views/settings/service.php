af0b5646ca773c4fa01e2cec6569850125197753
xzc <1805793040@qq.com>
2018-12-25 14:20:28 +0800

    update

--- application/views/settings/service.php
+++ application/views/settings/service.php
@@ -28,36 +28,194 @@ $(document).keydown(function(event) {
 </script>
 
 <style>
-.manage-wrap{margin: 0 auto;width: 300px;}
-.manage-wrap .ui-input{width: 200px;font-size:14px;}
-.manage-wrap .hideFeild{position: absolute;top: 30px;left:80px;width:210px;border:solid 1px #ccc;background-color
-:#fff;}
-.ztreeDefault{overflow-y:auto;max-height:240px;}
-.searchbox{float: left;font-size: 14px;}
-.searchbox li{float: left;margin-right: 10px;}
-#matchCon{width:140px;}
-.ui-input-ph {color: #aaa;}
-.cur #custom-assisting .ui-combo-wrap {background: #eaeaea;border-color: #c1c1c1;}
-.cur #custom-assisting input {background: #eaeaea;font-weight: bold;}
-.ui-droplist-wrap .selected {background-color: #d2d2d2;}
-.input-txt{font-size:14px;}
-.ui-droplist-wrap .list-item {font-size:14px;}
+    .clearfix::before,
+    .clearfix::after{
+        content:'';
+        display: block;
+        line-height: 0;
+        height: 0;
+        visibility: hidden;
+        clear: both;
+    }
+    .grid-wrap{
+        background-color: #fff;
+        border: 1px solid #ddd;
+        height: 800px;
+        width: 100%;
+        overflow: auto;
+        position: relative;
+    }
+    .table{
+        width: 100%;
+    }
+    table{
+        border-collapse:collapse;
+    }
+    .table tr{
+        border: 1px solid #000;
+        height: 33px;
+    }
+    .table th{
+        background-color: #f1f1f1;
+        height: 30px;
+    }
+    .table th,td{
+        border-right: 1px solid #e2e2e2;
+        border-bottom: 1px solid #e2e2e2;
+        border-top: 1px solid #fff;
+        border-left: 1px solid #fff;
+        width: 100px;
+        height: 33px;
+        text-align: center;
+    }
+    .table tr:hover{
+        background-color: #f8ff94;
+    }
+    .table td>span{
+        display: inline-block;
+        width: 100px;
+        height: 33px;
+        line-height: 33px;
+        margin-bottom: -6px;
+        overflow: hidden;
+        text-overflow:ellipsis;
+    }
+    #page{
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        background-color: #f1f1f1;
+        line-height: 30px;
+    }
+    #page>div{
+        float: left;
+        width: 33.333%;
+        text-align: center;
+    }
+    #page>div:last-child{
+        text-align: right;
+    }
+    .page_center>div{
+        float: left;
+        margin-left: 10px;
+    }
+    .page_center>div:first-child{
+        background-image: url(<?php echo base_url()?>statics/css/img/ui-icons_20150410.png);
+        background-repeat: no-repeat;
+        background-position: -48px 0px;
+        width: 16px;
+        height: 16px;
+        margin-top: 8px;
+    }
+    .page_center>div:nth-child(2){
+        background-image: url(<?php echo base_url()?>statics/css/img/ui-icons_20150410.png);
+        background-repeat: no-repeat;
+        background-position: -16px 0px;
+        width: 16px;
+        height: 16px;
+        margin-top: 8px;
+    }
+    .page_center>div:nth-child(3){
+        width: 42px;
+        height: 18px;
+    }
+    .page_center>div:nth-child(3)>input{
+        width: 100%;
+        height: 100%;
+    }
+    .page_center>div:nth-child(5){
+        background-image: url(<?php echo base_url()?>statics/css/img/ui-icons_20150410.png);
+        background-repeat: no-repeat;
+        background-position: 0px 0px;
+        width: 16px;
+        height: 16px;
+        margin-top: 8px;
+    }
+    .page_center>div:nth-child(6){
+        background-image: url(<?php echo base_url()?>statics/css/img/ui-icons_20150410.png);
+        background-repeat: no-repeat;
+        background-position: -32px 0px;
+        width: 16px;
+        height: 16px;
+        margin-top: 8px;
+    }
 </style>
 </head>
 <body>
 <div class="wrapper">
-	<div class="mod-toolbar-top cf">
-	    <div class="fr"><a href="#" class="ui-btn ui-btn-sp mrb" id="btn-add">新增</a><a class="ui-btn" id
-="btn-refresh">刷新</a></div>
+	<div class="mod-search cf">
+	    <div class="fr">
+            <a href="javascript:void(0);" class="ui-btn ui-btn-sp mrb">新增</a>
+            <a href="javascript:void(0);" class="ui-btn" id="btn-batchDel">刷新</a>
+        </div>
 	  </div>
     <div class="grid-wrap">
-	    <table id="grid">
-	    </table>
-	    <div id="page"></div>
-	  </div>
+        <div class="table">
+            <table>
+                <thead>
+                    <tr>
+                        <th style="width: 65px;">操作</th>
+                        <th style="width: 205px;">类别</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><span>2hgtr4weh4efe3gerrgrwhtbvrgweh56t56t4</span></td>
+                        <td><span>2hgtr4weh4efe3gerrgrwhtbvrgweh56t56t4</span></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+	    <div id="page">
+            <div class="page_left">&nbsp;</div>
+            <div class="page_center">
+                <div></div>
+                <div></div>
+                <div>
+                    <input type="text" value="1">
+                </div>
+                <div>共 1 页</div>
+                <div></div>
+                <div></div>
+                <div>
+                    <select name="pages" id="pages">
+                        <option value="100">100</option>
+                        <option value="200">200</option>
+                        <option value="300">300</option>
+                    </select>
+                </div>
+            </div>
+            <div class="page_right">1 -  1 &nbsp;&nbsp; 共  1  条</div>
+        </div>
+    </div>
 </div>
-<script src="<?php echo base_url()?>statics/js/dist/categoryList.js?ver=20150522"></script>
 
+<script>
+    $(function () {
+        
+    });
+</script>
+<script>
+    Public.pageTab();
+    reportParam();
+    function reportParam(){
+        $("[tabid^='report']").each(function(){
+            var dateParams = "beginDate="+parent.SYSTEM.beginDate+"&endDate="+parent.SYSTEM.endDate;
+            var href = this.href;
+            href += (this.href.lastIndexOf("?")===-1) ? "?" : "&";
+            if($(this).html() === '商品库存余额表'){
+                this.href = href + "beginDate="+parent.SYSTEM.startDate+"&endDate="+parent.SYSTEM.endDate;
+            }
+            else{
+                this.href = href + dateParams;
+            }
+        });
+    }
+
+    var goodsCombo = Business.goodsCombo($('#goodsAuto'), {
+        extraListHtml: ''
+    });
+</script>
 </body>
 </html>
 
